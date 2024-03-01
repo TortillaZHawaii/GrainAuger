9b84c13c10c07994b3cbde77449bdba8bd7cbdd8
Dawid Wysocki <tortillazhawaii@gmail.com>
2024-03-01 14:20:34 +0100

    Add IAugerGrain interface

---
+++ Examples/GrainAuger.Examples.Abstractions/IAugerGrain.cs
@@ -0,0 +1,8 @@
+﻿namespace GrainAuger.Examples.Abstractions;
+
+public interface IAugerGrain
+{
+    public Task StartAsync(string providerName, string streamNamespace, Guid inputStreamGuid,
+        Guid outputStreamGuid);
+    public Task StopAsync();
+}
