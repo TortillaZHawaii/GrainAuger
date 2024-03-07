ec3c5320014932fd06e18ac97d8707075513ef79
Dawid Wysocki <tortillazhawaii@gmail.com>
2024-03-07 18:31:41 +0100

    Run WebApi through Aspire

--- Examples/GrainAuger.Examples.Aspire/Program.cs
+++ Examples/GrainAuger.Examples.Aspire/Program.cs
@@ -1,6 +1,14 @@
+using Projects;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddKafka("messaging");
-builder.AddRedis("membership");
+var messaging = builder.AddKafka("messaging");
+var membership = builder.AddRedis("membership");
+var storage = builder.AddRedis("storage");
 
+builder.AddProject<GrainAuger_Examples_WebApi>("silo1")
+    // .WithReference(messaging)
+    .WithReference(membership)
+    .WithReference(storage);
+    
 builder.Build().Run();
--- Examples/GrainAuger.Examples.WebApi/Program.cs
+++ Examples/GrainAuger.Examples.WebApi/Program.cs
@@ -1,4 +1,5 @@
 using GrainAuger.Examples.Grains;
+using Orleans.Streams.Kafka.Config;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,16 +7,66 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 string providerName = "StreamProvider";
+string streamNamespace = "Auger";
+
+var membershipConnectionString = builder.Configuration.GetConnectionString("membership");
+var storageConnectionString = builder.Configuration.GetConnectionString("storage");
+var messagingConnectionString = builder.Configuration.GetConnectionString("messaging");
+
+var grainStorageRedisOptions = new StackExchange.Redis.ConfigurationOptions
+{
+    EndPoints = {storageConnectionString ?? ""}
+};
 
 builder.Host.UseOrleans(siloBuilder =>
 {
-    siloBuilder.UseLocalhostClustering();
-    siloBuilder.AddMemoryGrainStorageAsDefault();
-    siloBuilder
-        .AddMemoryGrainStorage("Auger");
-    siloBuilder
-        .AddMemoryGrainStorage("PubSubStore")
-        .AddMemoryStreams(providerName);
+    if (membershipConnectionString != null)
+    {
+        siloBuilder.UseRedisClustering(membershipConnectionString);
+    }
+    else
+    {
+        siloBuilder.UseLocalhostClustering();
+    }
+
+    if (storageConnectionString != null)
+    {
+        siloBuilder
+            .AddRedisGrainStorage("Auger", options =>
+                options.ConfigurationOptions = grainStorageRedisOptions)
+            .AddRedisGrainStorage("PubSubStore", options =>
+                options.ConfigurationOptions = grainStorageRedisOptions);
+    }
+    else
+    {
+        siloBuilder
+            .AddMemoryGrainStorage("Auger")
+            .AddMemoryGrainStorage("PubSubStore");
+    }
+    
+    if (messagingConnectionString != null)
+    {
+        siloBuilder.AddKafka(providerName)
+            .WithOptions(options =>
+            {
+                options.BrokerList = new[] { messagingConnectionString };
+                options.ConsumerGroupId = streamNamespace;
+                options.ConsumeMode = ConsumeMode.StreamEnd;
+
+                options
+                    .AddTopic(streamNamespace, new TopicCreationConfig
+                    {
+                        AutoCreate = true, Partitions = 2, ReplicationFactor = 1
+                    });
+            })
+            .AddJson()
+            .AddLoggingTracker();
+    }
+    else
+    {
+        siloBuilder.AddMemoryStreams(providerName);
+    }
+    
     siloBuilder.UseDashboard(x => x.HostSelf = true);
 });
 
@@ -40,25 +91,27 @@ string producerKey = "Producer";
 string distinctKey = "Distinct";
 string chunkKey = "Chunk";
 
-string streamNamespace = "Auger";
 
 app.MapGet("/start", async (IGrainFactory factory) =>
 {
     var producerGrain = factory.GetGrain<IProducerGrain>(producerKey);
     await producerGrain.StartAsync(providerName, streamNamespace, producerStreamGuid);
     
+    // split the stream into two streams
+    // stream for distinct
     var distinctAugerGrain = factory.GetGrain<IDistinctAuger<int>>(distinctKey);
     await distinctAugerGrain.StartAsync(providerName, streamNamespace,
         producerStreamGuid, distinctStreamGuid);
-
+    
+    // stream for select, where, chunk
     var selectAugerGrain = factory.GetGrain<ISelectAuger<int, int>>(selectorKey);
     await selectAugerGrain.StartAsync(providerName, streamNamespace,
-        distinctStreamGuid, selectorStreamGuid);
+        producerStreamGuid, selectorStreamGuid);
     
     var whereAugerGrain = factory.GetGrain<IWhereAuger<int>>(whereKey);
     await whereAugerGrain.StartAsync(providerName, streamNamespace,
         selectorStreamGuid, whereStreamGuid);
-    
+
     var chunkAugerGrain = factory.GetGrain<IChunkAuger<int>>(chunkKey);
     await chunkAugerGrain.StartAsync(providerName, streamNamespace,
         whereStreamGuid, chunkStreamGuid);
