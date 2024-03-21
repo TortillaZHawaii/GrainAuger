271e8be473428882a078b08a95acd242105c40f0
Dawid Wysocki <tortillazhawaii@gmail.com>
2024-03-21 15:56:42 +0100

    Use ints inside RoundRobinLoadBalancerAuger

--- Examples/FraudDetection/GrainAuger.Examples.FraudDetection.WebApi/GenerateBefore/LoadBalancers/RoundRobinLoadBalancerAuger.cs
+++ Examples/FraudDetection/GrainAuger.Examples.FraudDetection.WebApi/GenerateBefore/LoadBalancers/RoundRobinLoadBalancerAuger.cs
@@ -4,35 +4,23 @@ namespace GrainAuger.Examples.FraudDetection.WebApi.GenerateBefore.LoadBalancers
 
 public class RoundRobinLoadBalancerAuger<T> : Auger<T, T>
 {
-    private readonly Guid[] _pool;
+    public int PoolSize { get; }
     private int _index = 0;
     
     public RoundRobinLoadBalancerAuger(int poolSize)
     {
-        _pool = new Guid[poolSize];
-        for (var i = 0; i < poolSize; i++)
-        {
-            _pool[i] = Int2Guid(i);
-        }
+        PoolSize = poolSize;
     }
     
     public override async Task ProcessAsync(T input, Func<T, Task> collect)
     {
         // we need to change the output stream depending on the pool
-        var guid = _pool[_index];
-        _index = _index + 1 >= _pool.Length ? 0 : _index + 1;
+        _index = _index + 1 >= PoolSize ? 0 : _index + 1;
         throw new NotImplementedException("This is a placeholder for the actual implementation");
-        var outputStreamId = StreamId.Create("GrainAuger_XXX", guid);
+        var outputStreamId = StreamId.Create("GrainAuger_XXX", _index);
         // we would need to punch a hole in the grain to get the stream provider
         // and use output stream instead of collect
         //var outputStream = GetStreamProvider("AugerStreamProvider").GetStream<T>(outputStreamId);
         //await outputStream.OnNextAsync(input);
-    }
-    
-    private static Guid Int2Guid(int value)
-    {
-        var bytes = new byte[16];
-        BitConverter.GetBytes(value).CopyTo(bytes, 0);
-        return new Guid(bytes);
     }
 }
