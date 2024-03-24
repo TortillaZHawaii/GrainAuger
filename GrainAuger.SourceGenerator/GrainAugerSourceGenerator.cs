21700b752ae9af4861df077622d521398f67eabc
Dawid Wysocki <tortillazhawaii@gmail.com>
2024-03-24 19:48:46 +0100

    Add Source Generator

---
+++ GrainAuger.SourceGenerator/GrainAugerSourceGenerator.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Linq;
+
+namespace GrainAuger.SourceGenerator;
+
+[Generator]
+public class GrainAugerSourceGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        // Filter the classes that implement IAugerJobConfiguration
+        context.SyntaxProvider
+            .CreateSyntaxProvider((node, token) =>
+        {
+            if (node is ClassDeclarationSyntax interfaceDeclaration)
+            {
+                if (interfaceDeclaration.BaseList?.Types.Any(x => x.Type.ToString() == "IAugerJobConfiguration") == true)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }, (generatorContext, cancelletionToken) => { return "Found"; });
+    }
+}
