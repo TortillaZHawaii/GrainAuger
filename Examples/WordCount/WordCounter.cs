a34a9e33134787fdc4c65e3273af7cf798c60d7d
Dawid Wysocki <tortillazhawaii@gmail.com>
2024-04-15 12:51:04 +0200

    Add logger to wordCounter

--- Examples/WordCount/WordCounter.cs
+++ Examples/WordCount/WordCounter.cs
@@ -2,14 +2,17 @@ using Orleans.Streams;
 
 namespace WordCount;
 
-class WordCounter(IAsyncObserver<int> output) : IAsyncObserver<string>
+class WordCounter(IAsyncObserver<int> output, ILogger<WordCounter> logger
+    ) : IAsyncObserver<string>
 {
     private int _wordCount = 0;
     
     public async Task OnNextAsync(string item, StreamSequenceToken? token = null)
     {
-        _wordCount += item.Split(' ').Length;
+        int length = item.Split(' ').Length;
+        _wordCount += length;
         await output.OnNextAsync(_wordCount);
+        logger.LogInformation("Word count for text \"{Text}\": {Length}, Total WordCount: {WordCount}", item, length, _wordCount);
     }
 
     public Task OnCompletedAsync()
