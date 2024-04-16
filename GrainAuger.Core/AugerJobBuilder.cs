405c6c378258c33ccabed540a10666f0e6386a61
Dawid Wysocki <tortillazhawaii@gmail.com>
2024-04-16 20:55:01 +0200

    Add an example to README

--- GrainAuger.Core/AugerJobBuilder.cs
+++ GrainAuger.Core/AugerJobBuilder.cs
@@ -4,9 +4,10 @@ namespace GrainAuger.Core
 {
     public class AugerJobBuilder : IAugerJobBuilder
     {
-        public IAugerStream FromStream<T>(string providerName, string streamName)
+        public IAugerStream FromStream<TData, TKey>(string providerName, string streamName)
+            where TData : notnull where TKey : notnull
         {
-            return new AugerStream(providerName, streamName, typeof(T));
+            return new AugerStream(providerName, streamName, typeof(TData));
         }
     }
 }
