cbab88f13ece59ecd04d70287c8e514c8e9b23ce
Dawid Wysocki <tortillazhawaii@gmail.com>
2024-04-21 12:58:25 +0200

    Remove aspire example

--- Examples/GrainAuger.Examples.Abstractions/IAugerGrain.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GrainAuger.Examples.Abstractions;
-
-public interface IAugerGrain : IGrainWithStringKey
-{
-    public Task StartAsync(string providerName, string streamNamespace, Guid inputStreamGuid,
-        Guid outputStreamGuid);
-    public Task StopAsync();
-}
