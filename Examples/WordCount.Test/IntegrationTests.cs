4a7906a103c9d2059d9b4af67789996fa00b8fb8
Dawid Wysocki <tortillazhawaii@gmail.com>
2024-12-09 19:37:29 +0100

    Fix integration test

--- Examples/WordCount.Test/IntegrationTests.cs
+++ Examples/WordCount.Test/IntegrationTests.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Orleans.Streams;
 using Orleans.TestingHost;
@@ -6,16 +7,27 @@ namespace WordCount.Test;
 
 public class IntegrationTests
 {
-    public async Task<TestCluster> SetUp()
+    private static async Task<TestCluster> SetUp()
     {
         var builder = new TestClusterBuilder()
-            .AddSiloBuilderConfigurator<SiloConfigurator>();
+            .AddSiloBuilderConfigurator<SiloConfigurator>()
+            .AddClientBuilderConfigurator<ClientConfigurator>();
         var cluster = builder.Build();
         await cluster.DeployAsync();
         return cluster;
     }
-    
-    public async Task TearDown(TestCluster cluster)
+
+    private static async Task<TestCluster> SetUpDisabled()
+    {
+        var builder = new TestClusterBuilder()
+            .AddSiloBuilderConfigurator<SiloConfiguratorWithFlagDisabled>()
+            .AddClientBuilderConfigurator<ClientConfigurator>();
+        var cluster = builder.Build();
+        await cluster.DeployAsync();
+        return cluster;
+    }
+
+    private static async Task TearDown(TestCluster cluster)
     {
         await cluster.StopAllSilosAsync();
     }
@@ -40,10 +52,37 @@ public class IntegrationTests
             await inputStream.OnNextAsync(item);
         }
 
-        await Task.Delay(1000);
+        await Task.Delay(400);
         
         // Assert
-        Assert.AreEqual(mockReader.ReceivedItems.ToArray(), expectedCounts);
+        Assert.That(mockReader.ReceivedItems.ToArray(), Is.EqualTo(expectedCounts));
+        await TearDown(cluster);
+    }
+
+    [TestCase(new [] { "Hello", "World" }, 0)]
+    [TestCase(new[] { "Hello", "World", "this", "is", "a", "test" }, 0)]
+    [TestCase(new string[0], 0)]
+    [TestCase(new[] { "Five words in this sentence", "Then extra three" }, 0)]
+    public async Task TestWordCountWithFeatureFlagDisabled(string[] items, int size)
+    {
+        // Arrange
+        var cluster = await SetUpDisabled();
+        var streamProvider = cluster.Client.GetStreamProvider("MemoryStream");
+        var inputStream = streamProvider.GetStream<string>(StreamId.Create("WordCountInput", "abc"));
+        var outputStream = streamProvider.GetStream<int>(StreamId.Create("countStream", "abc"));
+        var mockReader = new MockReader<int>();
+        await outputStream.SubscribeAsync(mockReader);
+
+        foreach (var item in items)
+        {
+            // Act
+            await inputStream.OnNextAsync(item);
+        }
+
+        await Task.Delay(400);
+        
+        // Assert
+        Assert.That(mockReader.ReceivedItems, Is.Empty);
         await TearDown(cluster);
     }
 }
@@ -64,9 +103,37 @@ file class SiloConfigurator : ISiloConfigurator
     }
 }
 
+file class SiloConfiguratorWithFlagDisabled : ISiloConfigurator
+{
+    public void Configure(ISiloBuilder hostBuilder)
+    {
+        hostBuilder.ConfigureServices(services =>
+        {
+            var service = new FeatureFlagService
+            {
+                Enabled = false
+            };
+            services.AddSingleton<IFeatureFlagService>(service);
+        });
+        hostBuilder
+            .AddMemoryGrainStorage("PubSubStore")
+            .AddMemoryGrainStorageAsDefault()
+            .AddStreaming();
+        hostBuilder.AddMemoryStreams("MemoryStream");
+    }
+}
+
+file class ClientConfigurator : IClientBuilderConfigurator
+{
+    public void Configure(IConfiguration configuration, IClientBuilder clientBuilder)
+    {
+        clientBuilder.AddMemoryStreams("MemoryStream");
+    }
+}
+
 file class MockReader<T> : IAsyncObserver<T>
 {
-    public List<T> ReceivedItems { get; } = new();
+    public List<T> ReceivedItems { get; } = [];
     
     public Task OnNextAsync(T item, StreamSequenceToken? token = null)
     {
